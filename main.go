646b23cacff9f340e42dfc762a860a7f40a4221a
Will Rowe <will.rowe@stfc.ac.uk>
2019-07-14 21:29:31 +0100

    moving codebase to UKRI and wiping repo

--- main.go
+++
@@ -1,9 +0,0 @@
-// Copyright © 2018 Science and Technology Facilities Council (UK) <will.rowe@stfc.ac.uk>
-
-package main
-
-import "github.com/will-rowe/hulk/cmd"
-
-func main() {
-	cmd.Execute()
-}
